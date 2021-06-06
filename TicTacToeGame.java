75e5c16d45718e47123e385913c8fb126a68a23a
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-07 02:24:10 +0530

    Added: Created a TicTacToe board with empty spaces

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,8 +1,19 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class TicTacToeGame {
+    static Scanner scanner = new Scanner(System.in);
+    static char[] board = new char[10];
 
-    public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe Game");
+    public static void main(String[] args){
+        createBoard();
+        System.out.println("Board is created");
     }
+    static void createBoard(){
+        //Initialize board array
+        for(int i=1; i<board.length; i++){
+            board[i] =' ';
+        }
+    }
+
 }
