54b522a613d670959279ba1fcb9ae1a65f6e8e6b
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-07 02:37:20 +0530

    Added: User input X or O is taken and assigned

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,19 +1,48 @@
 package com.bridgelabz;
+
 import java.util.Scanner;
 
 public class TicTacToeGame {
     static Scanner scanner = new Scanner(System.in);
     static char[] board = new char[10];
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         createBoard();
-        System.out.println("Board is created");
+        char player = takeUserInput();
+        System.out.println("You chose " + player);
+        char computer;
+        if ((player == 'X')) {
+            computer = 'O';
+        } else {
+            computer = 'X';
+        }
+        System.out.println("Computer is assigned with " + computer);
     }
-    static void createBoard(){
-        //Initialize board array
-        for(int i=1; i<board.length; i++){
-            board[i] =' ';
+
+    /**
+     * A board is created of char[] size 10
+     * Empty spaces are assigned
+     * 0Th index is ignored
+     */
+    static void createBoard() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
         }
     }
 
+    /**
+     * choose X OR O
+     * Player and Computer letter is determined
+     *
+     * @return Input from the user
+     */
+    static char takeUserInput() {
+        System.out.println("Input letter X or O.\n" +
+                "Please not that the letters are case sensitive.");
+        char input = scanner.next().charAt(0);
+        return input;
+    }
+
 }
+
+
