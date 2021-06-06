8e491235d7bfe47986968485f17e8ed3132f9284
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-07 03:47:01 +0530

    Added: After every move check if winner or tie or change the turn

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,5 @@
 package com.bridgelabz;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToeGame {
@@ -17,11 +16,66 @@ public class TicTacToeGame {
         } else {
             computer = 'X';
         }
-        System.out.println("Computer is assigned with " + computer);
+        System.out.println("Computer chose " + computer);
+        while (true) {
+            int position = selectPosition(); //Select user position
+            boolean playStatus = makeAMove(player, position, "User"); //Validate user position
+            if (playStatus == true) { //Exit if user won or game over.
+                break;
+            }
+            position = setComputerPosition(computer); //Select computer position
+            playStatus = makeAMove(computer, position, "Computer"); //Validate computer position
+            if (playStatus == true) { //Exit if computer won or game over.
+                break;
+            }
+        }
+    }
+
+    /**
+     * check if free space to make a move
+     *
+     * @param value    entered by user or computer
+     * @param position position on board
+     * @param name     user or computer
+     */
+    static boolean makeAMove(char value, int position, String name) {
+        if (position <= 0) { //No position left to play.
+            System.out.println("Game Over");
+            return true;
+        }
+        System.out.println(name + " chose " + position);
+        board[position] = value; //Set value at selected cell
+        boolean win = isWin(board, value); //Check if won
+        if (win) {
+            System.out.println(name + " won!!!");
+            return true;
+        }
         showBoard();
-        int position = selectPosition(); //Select user position
-        makeAMove(player, position, "User"); //Validate user position
-        boolean userWonTheToss = tossTheCoin();
+        return false;
+    }
+
+    static boolean isWin(char[] arr, char value) {
+        if ((arr[1] == value) &&                            //1st cell of 1st row
+                ((arr[2] == value && arr[3] == value)           //2nd and 3rd cell of 1st row (Horizontal)
+                        || (arr[4] == value && arr[7] == value) // 1st column (Vertical)
+                        || (arr[5] == value && arr[9] == value) // Diagonal
+                )) {
+            return true;
+        }
+        if (arr[2] == value && arr[5] == value && arr[8] == value) {
+            return true;
+        }
+        if (arr[3] == value && ((arr[6] == value && arr[9] == value)
+                || arr[5] == value && arr[7] == value)) {
+            return true;
+        }
+        if (arr[4] == value && arr[5] == value && arr[6] == value) {
+            return true;
+        }
+        if (arr[7] == value && arr[8] == value && arr[9] == value) {
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -34,6 +88,61 @@ public class TicTacToeGame {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
+    }
+
+    static int setComputerPosition(char computer) {
+        //Clone board array
+        char[] arr = new char[board.length];
+        for (int i = 1; i < board.length; i++) {
+            arr[i] = board[i];
+        }
+        //Put values into cloned board and check if computer can win.
+        int index = simulateWin(arr, computer);
+        if ((index > 0)) { //Computer can win if value is assigned at idx position.
+            board[index] = computer;
+            return index;
+        }
+        char user;
+        if (computer == 'X') { //Guess the  user value
+            user = 'O';
+        } else {
+            user = 'X';
+        }
+        //Find out the next position where user can win and block it
+        index = simulateWin(arr, user);
+        if (index > 0) {
+            board[index] = computer;
+            return index;
+        }
+        //Since no winning position is available, fill next empty cell
+        for (int i = 1; i < board.length; i++) {
+            if (board[i] == ' ') {
+                board[i] = computer;
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    static int simulateWin(char[] arr, char value) {
+        //Iterate through all the cells and check if can be won.
+        for (int i = 1; i < 10; i++) {
+            //Copy value to temporary buffer.
+            char initialValue = arr[i];
+            //Check if position is empty
+            if (arr[i] == ' ') {
+                //Since position is empty, assign value.
+                arr[i] = value;
+            }
+            //Check if we can win.
+            if (isWin(arr, value)) {
+                return i;
+            }
+            //Restore value back
+            arr[i] = initialValue;
+        }
+        //No win
+        return -1;
     }
 
     /**
@@ -53,24 +162,10 @@ public class TicTacToeGame {
         return input;
     }
 
-
-    /**
-     * Prints a current board
-     */
-    static void showBoard() {
-        System.out.println("_______");
-        for (int i = 1; i < board.length; i += 3) {
-            System.out.print("|");
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i + j] + "|");
-            }
-            System.out.println();
-            System.out.println("_______");
-        }
-    }
     /**
      * select position between 1 and 10
      * Ensure the index is free to select
+     *
      * @return Selected position
      */
     static int selectPosition() {
@@ -86,54 +181,19 @@ public class TicTacToeGame {
         return input;
     }
 
-
     /**
-     * check if free space to make a move
-     *
-     * @param value    entered by user or computer
-     * @param position position on board
-     * @param name     user or computer
+     * Prints a current board
      */
-    static void makeAMove(char value, int position, String name) {
-        if (position <= 0) { //No position left to play.
-            System.out.println("Game Over");
-            return;
+    static void showBoard() {
+        System.out.println("_______");
+        for (int i = 1; i < board.length; i += 3) {
+            System.out.print("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i + j] + "|");
+            }
+            System.out.println();
+            System.out.println("_______");
         }
-        System.out.println(name + " chose " + position);
-        board[position] = value; //Set value at selected cell
-    }
-
-    /**
-     * Tossing a coin
-     * Allowing the user to opt heads or tails
-     * assigning accordingly who plays first, computer or user
-     * @returns who won the toss
-     */
-    static boolean tossTheCoin() {
-        System.out.println("1 means head, 0 means tail.");
-        System.out.println("Please enter 1 for head, 0 for tail.");
-        int userValue = scanner.nextInt();
-        if (userValue == 0) {
-            System.out.println("You chose tail.");
-        } else if (userValue == 1) {
-            System.out.println("You chose head");
-        } else {
-            System.out.println("You must chose either 0 or 1.");
-            return tossTheCoin();
-        }
-        System.out.println("Tossing the coin.....");
-        int toss = new Random().nextInt() * 2;
-        if (toss == 0) {
-            System.out.println("It's tail.");
-        } else {
-            System.out.println("It's head.");
-        }
-        if (userValue == toss) {
-            System.out.println("You won the toss.");
-            return true;
-        } else {
-            System.out.println("Computer won the toss.");
-        }
-        return false;
     }
 }
+
